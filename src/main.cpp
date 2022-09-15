b7e5fc28b1e8d4af07b5f5b5f20963d4498dbb42
Juan Lugo Sánchez <60227071+JuanLugoS@users.noreply.github.com>
2022-09-14 22:31:14 -0500

    Cambios en estructura de directorios y pruebas de modelo

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,40 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#include <telemetria/prueba.cpp>
+#include <utilities/displayHandler.cpp>
+
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+
+DisplayHandler displayHandler(0);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(9600);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  display.clearDisplay();
+  display.setCursor(0,0); // Start at top-left corner
+  
+  displayHandler.write(display, "Wave",0,0,3);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-   
-  Serial.println("Hello World!");
+  //delay(500);
+  //Serial.println("Hello World!");
 }
