c8448baea9ab8bddf87e1555c1d3d1db659a78b4
Rene Fonseca <rf.fonseca@uniandes.edu.co>
2024-03-16 10:54:43 -0500

    condicional para que los servos sirvan

--- src/main.cpp
+++ src/main.cpp
@@ -633,6 +633,7 @@ void setup() {
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
+    
   }
   
   pca9685.begin();
